146c14af957bdbde33797dc23299537320200925
Angelo Basa <ambasa@uci.edu>
2020-05-11 19:11:53 -0700

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Angelo Basa on 5/11/20.
+//  Copyright © 2020 Angelo Basa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
